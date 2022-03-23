b7c8f69cb78de8baa90c2c013f28a3e498c72d17
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2022-03-23 15:33:06 +0500

    added Max algoritm class

---
+++ Max.cs
@@ -0,0 +1,22 @@
+﻿namespace Algorithms
+{
+    public static class Max
+    {
+        /// <summary>
+        /// Get largest value from integer array
+        /// </summary>
+        /// <param name="array">Integer array</param>
+        /// <returns>Largest value from array</returns>
+        public static int GetMax(int[] array)
+        {
+            var tempMax = 0;
+            foreach (var item in array)
+            {
+                if (tempMax < item)
+                    tempMax = item;
+            }
+
+            return tempMax;
+        }
+    }
+}
