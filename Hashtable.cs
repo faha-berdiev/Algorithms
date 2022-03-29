0517c81d60e3ac7e18b7b8a31c3c0cc30caaea13
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2022-03-29 19:57:18 +0500

    added hastable

---
+++ Hashtable.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Algorithms
+{
+    public class HashtableEntry
+    {
+        public HashtableEntry(object key, object value)
+        {
+            Key = key;
+            Value = value;
+        }
+
+        public object Key { get; set; }
+        public object? Value { get; set; }
+    }
+    
+    public class Hashtable
+    {
+        private readonly int _maxCount = 0;
+        private HashtableEntry[] _hashtableEntries;
+            
+        public Hashtable(int maxCount = 1000)
+        {
+            _maxCount = maxCount;
+            _hashtableEntries = new HashtableEntry[maxCount];
+        }
+
+        public object? Get(object key)
+        {
+            var hashCodeIndex = Math.Abs(key.GetHashCode() % _maxCount);
+            return _hashtableEntries[hashCodeIndex]?.Value;
+        }
+
+        public void Put(object key, object? value)
+        {
+            var hashCodeIndex = Math.Abs(key.GetHashCode() % _maxCount);
+            var hashTableEntry = _hashtableEntries[hashCodeIndex];
+            if (hashTableEntry == null)
+            {
+                _hashtableEntries[hashCodeIndex] = new HashtableEntry(key, value);
+                return;
+            }
+
+            if (hashTableEntry.Key == key)
+            {
+                hashTableEntry.Value = value;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,6 +6,13 @@ namespace Algorithms
     {
         public static void Main()
         {
+            Hashtable hashtable = new Hashtable();
+            var value = hashtable.Get("Faridun");
+            Console.WriteLine(value);
+            
+            hashtable.Put("Faridun", "Berdiev");
+            value = hashtable.Get("Faridun");
+            Console.WriteLine(value);
         }
     }
 }
